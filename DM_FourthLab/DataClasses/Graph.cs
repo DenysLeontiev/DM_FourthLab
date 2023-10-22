b78992d9ee80744f31dfcae503f2e75f6f846ffd
Denys Leon <leonden0046@gmail.com>
2023-10-22 23:03:38 +0300

    Add project files.

---
+++ DM_FourthLab/DataClasses/Graph.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_FourthLab.DataClasses
+{
+    public class Graph
+    {
+        private List<int>[] adjacent;
+        private List<List<int>> allPaths;
+
+        public Graph(int v)
+        {
+            adjacent = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adjacent[i] = new List<int>();
+        }
+
+        public void addEdge(int v, int w)
+        {
+            adjacent[v].Add(w);
+        }
+
+        // Метод для знаходження та виведення всіх шляхів між вершинами 's' та 'd'
+        public List<List<int>> findAllPaths(int s, int d)
+        {
+            allPaths = new List<List<int>>();
+            List<int> path = new List<int>();
+            path.Add(s); // Додаємо початкову вершину в поточний шлях
+            findAllPathsUtil(s, d, path);
+            return allPaths;
+        }
+
+        private void findAllPathsUtil(int u, int d, List<int> path)
+        {
+            // Якщо поточна вершина дорівнює цільовій вершині (знайдено шлях)
+            if (u == d)
+            {
+                allPaths.Add(new List<int>(path));
+                return;
+            }
+
+            // Рекурсивно перебираємо всі сусідні вершини
+            foreach (int i in adjacent[u])
+            {
+                if (!path.Contains(i)) // Якщо вершина 'i' ще не була включена до поточного шляху
+                {
+                    path.Add(i); // Додаємо вершину 'i' до поточного шляху
+                    findAllPathsUtil(i, d, path); // Рекурсивно продовжуємо пошук
+                    path.Remove(i); // Після завершення рекурсивного виклику видаляємо вершину 'i' з поточного шляху
+                }
+            }
+        }
+    }
+}
---
+++ DM_FourthLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using DM_FourthLab.DataClasses;
+using System;
+using System.Collections.Generic;
+
+namespace DM_FourthLab
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(5);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 2);
+            graph.addEdge(0, 3);
+            graph.addEdge(1, 3);
+            graph.addEdge(2, 3);
+            graph.addEdge(1, 4);
+            graph.addEdge(2, 4);
+
+            int source = 0;
+            int destination = 3;
+
+            List<List<int>> allPaths = graph.findAllPaths(source, destination);
+
+            Console.WriteLine("All paths from {0} to {1}:", source, destination);
+            foreach (var path in allPaths)
+            {
+                Console.WriteLine(string.Join(" -> ", path));
+            }
+        }
+    }
+}
